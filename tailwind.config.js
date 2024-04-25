9ddb8ebdbd9104bd2b7a1d4af0dc5b38c3f02430
marshall <ultramarshall@users.noreply.github.com>
2024-04-26 02:03:47 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  darkMode: false, 
+  theme: {
+    extend: {
+      colors: {
+        'bg-main': 'red',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
